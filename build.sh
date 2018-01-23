bb831abe4d0b24539a6a08421ca57da920d49369
jadilet <adilet.571@gmail.com>
2018-01-23 18:47:44 +0600

    Added ruby modules

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 
 rm subutai_cli-1.0.0.gem
 gem build subutai_cli.gemspec
+gem uninstall subutai_cli
+gem install ./subutai_cli-1.0.0.gem
 vagrant plugin uninstall subutai_cli
 vagrant plugin install ./subutai_cli-1.0.0.gem
