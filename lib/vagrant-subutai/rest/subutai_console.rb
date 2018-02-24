53f9eb8a3d8bee512c4069715ad91c2ff879c401
jadilet <adilet.571@gmail.com>
2018-02-24 11:12:45 +0600

    Added net read time out

--- lib/vagrant-subutai/rest/subutai_console.rb
+++ lib/vagrant-subutai/rest/subutai_console.rb
@@ -113,6 +113,7 @@ module VagrantSubutai
         https = Net::HTTP.new(uri.host, uri.port)
         https.use_ssl = true
         https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        https.read_timeout = 60 * 20 # 20 min
 
         request = Net::HTTP::Post.new(uri.request_uri)
         request.set_form_data({'command' => cmd, 'hostId' => hostId, 'path' => path, 'timeOut' => timeOut})
