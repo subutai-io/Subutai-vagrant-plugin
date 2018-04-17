e4896590a978d97bcc4e0f69ab3a58776cbfe731
jadilet <adilet.571@gmail.com>
2018-04-17 17:21:25 +0600

    parallels mac prefix added

--- lib/vagrant-subutai/packer/subutai_net.rb
+++ lib/vagrant-subutai/packer/subutai_net.rb
@@ -130,8 +130,8 @@ def random_mac_addr(provider)
       PROVIDER_MAC_PREFIXES[:vmware] + 3.times.map { '%02x' % rand(0..255) }.join
       #raise "Unsupported provider #{provider}"
     when :parallels
-      # PROVIDER_MAC_PREFIXES[:parallels] + 3.times.map { '%02x' % rand(0..255) }.join
-      raise "Unsupported provider #{provider}"
+      PROVIDER_MAC_PREFIXES[:parallels] + 3.times.map { '%02x' % rand(0..255) }.join
+      #raise "Unsupported provider #{provider}"
     when :hyper_v
       # PROVIDER_MAC_PREFIXES[:hyper_v] + 3.times.map { '%02x' % rand(0..255) }.join
     else
