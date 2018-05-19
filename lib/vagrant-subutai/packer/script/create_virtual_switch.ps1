620aaf8a9d38dfc65fdab59a4c80d9456fc6a086
jadilet <adilet.571@gmail.com>
2018-05-19 17:13:02 +0600

    added error handling to script

--- lib/vagrant-subutai/packer/script/create_virtual_switch.ps1
+++ lib/vagrant-subutai/packer/script/create_virtual_switch.ps1
@@ -1,12 +1,17 @@
-$adapters = Get-NetAdapter -Physical | where status -eq 'up'
+Try {
+  $adapters = Get-NetAdapter -Physical | where status -eq 'up'
 
-foreach ($adapter in $adapters) {
-  $switch = Hyper-V\Get-VMSwitch -SwitchType External | where { $_.NetAdapterInterfaceDescription -eq $adapter.InterfaceDescription }
-  if ($switch -eq $null) {
-    $switch = Hyper-V\New-VMSwitch -Name 'vagrant-subutai' -NetAdapterName $adapter.Name -AllowManagementOS $true -Notes 'Parent OS, VMs, WiFi'
-  }
+  foreach ($adapter in $adapters) {
+    $switch = Hyper-V\Get-VMSwitch -SwitchType External | where { $_.NetAdapterInterfaceDescription -eq $adapter.InterfaceDescription } 2>nul
+    if ($switch -eq $null) {
+      $switch = Hyper-V\New-VMSwitch -Name 'vagrant-subutai' -NetAdapterName $adapter.Name -AllowManagementOS $TRUE -Notes 'Parent OS, VMs, WiFi' 2>nul
+    }
 
-  if ($switch -ne $null) {
-    break
+    if ($switch -ne $null) {
+      break
+    }
   }
+}
+Catch {
+  echo "Failed to create Virtual Switch"
 }
