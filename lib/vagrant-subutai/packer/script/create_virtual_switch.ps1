c9687f61163b60655fa4c225d5db4d789b90d1f7
jadilet <adilet.571@gmail.com>
2018-05-21 09:25:07 +0600

    added error action silently to create virtual switch

--- lib/vagrant-subutai/packer/script/create_virtual_switch.ps1
+++ lib/vagrant-subutai/packer/script/create_virtual_switch.ps1
@@ -2,9 +2,9 @@ Try {
   $adapters = Get-NetAdapter -Physical | where status -eq 'up'
 
   foreach ($adapter in $adapters) {
-    $switch = Hyper-V\Get-VMSwitch -SwitchType External | where { $_.NetAdapterInterfaceDescription -eq $adapter.InterfaceDescription } 2>nul
+    $switch = Hyper-V\Get-VMSwitch -SwitchType External -ErrorAction SilentlyContinue | where { $_.NetAdapterInterfaceDescription -eq $adapter.InterfaceDescription }
     if ($switch -eq $null) {
-      $switch = Hyper-V\New-VMSwitch -Name 'vagrant-subutai' -NetAdapterName $adapter.Name -AllowManagementOS $TRUE -Notes 'Parent OS, VMs, WiFi' 2>nul
+      $switch = Hyper-V\New-VMSwitch -Name 'vagrant-subutai' -ErrorAction SilentlyContinue -NetAdapterName $adapter.Name -AllowManagementOS $TRUE -Notes 'Parent OS, VMs, WiFi'
     }
 
     if ($switch -ne $null) {
@@ -13,5 +13,5 @@ Try {
   }
 }
 Catch {
-  echo "Failed to create Virtual Switch"
+
 }
