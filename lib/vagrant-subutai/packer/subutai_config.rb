bf604f5cfe397ace6dcc93ba14f12b705d803b61
jadilet <adilet.571@gmail.com>
2018-09-25 11:16:27 +0600

    removed cdn #121

--- lib/vagrant-subutai/packer/subutai_config.rb
+++ lib/vagrant-subutai/packer/subutai_config.rb
@@ -444,39 +444,7 @@ module SubutaiConfig
   end
 
   def self.get_latest_id_artifact(owner, artifact_name)
-    if write?
-      begin
-        url = url_of_cdn + '/raw/info?owner=' + owner + '&name=' + artifact_name
-        uri = URI.parse(url)
-        https = Net::HTTP.new(uri.host, uri.port)
-        https.use_ssl = true
-        https.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        https.read_timeout = 3600 # an hour
-
-        request = Net::HTTP::Get.new(uri.request_uri)
-        response = https.request(request)
-
-        case response
-          when Net::HTTPOK
-            response = JSON.parse(response.body)
-            response[0]['id']
-          when Net::HTTPNotFound
-            Put.error "#{response.body} template name #{name}, owner #{owner}"
-          "" # send empty id. DON'T REMOVE
-        end
-      rescue Errno::ECONNREFUSED
-        Put.error "cdn.subutai.io:8338 connection refused"
-        "" # send empty id. DON'T REMOVE
-      rescue Errno::EHOSTUNREACH
-        Put.error "cdn.subutai.io:8338 unreachable"
-        "" # send empty id. DON'T REMOVE
-      rescue => e
-        Put.error e
-        "" # send empty id. DON'T REMOVE
-      end
-    else
-      ""  # send empty id. DON'T REMOVE
-    end
+    ""  # send empty id.
   end
 end
 
