2a76e6e3a78db6220ede49485dc696760a2b58e8
jadilet <adilet.571@gmail.com>
2018-11-09 16:40:33 +0600

    refactored build script

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 
 rm *.gem
 gem build vagrant-subutai.gemspec
+mv vagrant-subutai-*.gem vagrant-subutai.gem
 
 gem uninstall vagrant-subutai
-gem install ./vagrant-subutai-*.gem
+gem install ./vagrant-subutai.gem
 
 vagrant plugin uninstall vagrant-subutai
-vagrant plugin install ./vagrant-subutai-*.gem
+vagrant plugin install ./vagrant-subutai.gem
